4d4b48460d1c5b67305a03703bec2b9eda4787f4
Haven Barnes <haven38@gmail.com>
2017-12-31 14:24:58 -0600

    Initial Commit

---
+++ ContactSwipes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ContactSwipes
+//
+//  Created by Haven Barnes on 12/31/17.
+//  Copyright © 2017 Haven Barnes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
